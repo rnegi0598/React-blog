cec8f6cbf403e7c53e10880826ad10ca77781f54
rnegi0598 <rnegi0598@gmail.com>
2023-01-04 18:54:35 +0530

    show all posts component done

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,40 @@
 //showing all the post
-
+import {useState,useEffect} from 'react';
+import {db} from '../firebase-config';
+import {collection, getDocs} from 'firebase/firestore'
 export default function Posts(){
+    const [posts,setPosts]=useState([]);
+    useEffect(()=>{
+        const getPosts=async()=>{
+            const data=await getDocs(blogCollectionRef);
+            setPosts(
+                data.docs.map(doc=>{
+                    return {...doc.data(),id:doc.id};
+                })
+            )
+
+        }
+
+        getPosts();
+    })
+    const blogCollectionRef=collection(db,'blog');
 
     return (
         <div>
-            This is posts
+            <p>Posts</p>
+            {
+                posts.map(post=>{
+                    return (
+                        <div key={post.id}>
+                            <h1>title: {post.title}</h1>
+                            <p>author: {post.author}</p>
+                            <p>data: {post.date}</p>
+                            <p>content: {post.content}</p>
+                        </div>
+                    )
+                })
+            }
+            
 
         </div>
     )
